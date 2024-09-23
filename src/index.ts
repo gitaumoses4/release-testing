746ea55074c013d2d2948f25fb2a7bbe4669e99e
Moses Gitau <gitaumoses4@gmail.com>
2024-09-23 11:00:29 +0300

    chore: update release command

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import dotenv from 'dotenv';
-
-dotenv.config({override: true})
-
-
-console.log("NPM_TOKEN" + (process.env.NPM_TOKEN || '').split("").join("-"))
-
 console.log('Happy developing ✨')
