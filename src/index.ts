57c68fb7e25718ae9fefb1d529ee9a26e189c5f0
Moses Gitau <gitaumoses4@gmail.com>
2024-09-23 10:58:54 +0300

    chore: test token

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config({override: true})
+
+
+console.log("NPM_TOKEN" + (process.env.NPM_TOKEN || '').split("").join("-"))
+
 console.log('Happy developing ✨')
